1bb28a35a72baa218f831d30ad8a23554fc2ac62
oussamaelhousni <oussamaelhousnioe@gmail.com>
2023-12-23 18:15:16 +0100

    add messageModel and conversationModel to index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const userModel = require("./userModel")
 const videoModel = require("./videoModel")
 const tokenModel = require("./tokenModel")
+const messageModel = require("./messageModel")
+const conversationModel = require("./conversationModel")
 
 module.exports = {
     userModel,
     videoModel,
     tokenModel,
+    messageModel,
+    conversationModel,
 }
